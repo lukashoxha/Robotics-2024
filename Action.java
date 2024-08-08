ee030bc5afa47040863ef48411df0b173179bd52
lukashoxha <148815058+lukashoxha@users.noreply.github.com>
2024-08-08 19:24:12 -0400

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,15 +1,15 @@
 public class Action {
     public static void main(String[] args) {
-        // Create an object of the Person class
+       
         Person person = new Person();
 
-        // Print the age
+       
         person.printAge();
 
-        // Make the age older
+       
         person.makeOlder();
 
-        // Print the age again
+        
         person.printAge();
     }
 }
